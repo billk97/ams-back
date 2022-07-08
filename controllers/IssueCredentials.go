a6a4ca1f5e96fda31b36efa20d769193bfed2a24
billk97 <vasileios@konstantinou.dev>
2022-07-08 16:47:23 +0300

    add endpoint for getCredential records by id

--- controllers/IssueCredentials.go
+++ controllers/IssueCredentials.go
@@ -20,6 +20,7 @@ func CreateIssueCredentialController(r *gin.Engine) {
 	{
 		api.POST("/", handleIssueCredential)
 		api.GET("/", getCredentialsRecords)
+		api.GET("/:id", getCredentialsRecordsByConnectionId)
 	}
 }
 
@@ -59,11 +60,7 @@ func getCredentialsRecords(c *gin.Context) {
 }
 
 func getCredentialsRecordsByConnectionId(c *gin.Context) {
-	//id, err := strconv.Atoi(c.Param("id"))
-	//if err != nil {
-	//	c.JSON(400, err)
-	//	return
-	//}
+	id := c.Param("id")
 	resp, err := http.Get(issueCredentialUrl + "/records")
 	if err != nil {
 		apiError := utils.NewApiError("REQUEST_FAILED", err, "details")
@@ -78,7 +75,18 @@ func getCredentialsRecordsByConnectionId(c *gin.Context) {
 	}
 	jsonString := string(body)
 	// filter only those with id
-	var jsonMap map[string]interface{}
-	json.Unmarshal([]byte(jsonString), &jsonMap)
-	c.JSON(200, jsonMap)
+	dto := dtos.CredentialExchangeRecordDTO{}
+	json.Unmarshal([]byte(jsonString), &dto)
+	credentialExchanges := dto.Results
+	var credentialExchangesForId []dtos.CredentialExchangeRecord
+	for i := range credentialExchanges {
+		if credentialExchanges[i].CredExRecord.ConnectionId != id {
+			continue
+		}
+		credentialExchangesForId = append(credentialExchangesForId, credentialExchanges[i])
+	}
+	result := dtos.CredentialExchangeRecordDTO{
+		credentialExchangesForId,
+	}
+	c.JSON(200, &result)
 }
