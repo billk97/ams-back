5990f243d9b3a0909dd93d2860cc5bec5874edb0
billk97 <vasileios@konstantinou.dev>
2022-07-25 11:53:44 +0300

    attempt to change the contexts if falls go two commits back

--- dtos/Person.go
+++ dtos/Person.go
@@ -5,20 +5,53 @@ type AlphaCorpEmployeeDTO struct {
 }
 
 type AlphaCorpEmployeeContext struct {
+	Version           float64     `json:"@version"`
+	Protected         bool        `json:"@protected"`
+	Emp               string      `json:"emp"`
+	Schema            string      `json:"schema"`
+	GivenName         string      `json:"givenName"`
+	FamilyName        string      `json:"familyName"`
+	JobTitle          string      `json:"jobTitle"`
+	Email             string      `json:"email"`
+	Id                string      `json:"id"`
+	Type              string      `json:"type"`
+	AtId              string      `json:"@id"`
+	AlphaCorpEmployee interface{} `json:"AlphacorpCredential"`
+}
+
+type SubCred struct {
+	AtId    string        `json:"@id"`
+	Context CustomContext `json:"@context"`
+}
+
+type CustomContext struct {
 	Version    float64 `json:"@version"`
 	Protected  bool    `json:"@protected"`
-	Emp        string  `json:"emp"`
+	Id         string  `json:"id"`
+	Type       string  `json:"type"`
 	Schema     string  `json:"schema"`
 	GivenName  string  `json:"givenName"`
 	FamilyName string  `json:"familyName"`
 	JobTitle   string  `json:"jobTitle"`
 	Email      string  `json:"email"`
-	Id         string  `json:"id"`
-	Type       string  `json:"type"`
-	atId       string  `json:"@id"`
 }
 
 func NewPersonContext() AlphaCorpEmployeeDTO {
+	custom := CustomContext{
+		1.1,
+		true,
+		"@id",
+		"@type",
+		"http://schema.org/",
+		"schema:givenName",
+		"schema:familyName",
+		"schema:jobTitle",
+		"schema:email",
+	}
+	sub := SubCred{
+		"https://api.alphacorp.vsk.gr/contexts/alphacorp-employee#AlphacorpCredential",
+		custom,
+	}
 	context := AlphaCorpEmployeeContext{
 		1.1,
 		true,
@@ -31,6 +64,7 @@ func NewPersonContext() AlphaCorpEmployeeDTO {
 		"@id",
 		"@type",
 		"https://api.alphacorp.vsk.gr/contexts/alphacorp-employee#AlphacorpCredential",
+		sub,
 	}
 	dto := AlphaCorpEmployeeDTO{
 		context,
